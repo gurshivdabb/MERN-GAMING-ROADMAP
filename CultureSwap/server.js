f2fd3ff45710b86deda78a0381a32aba614bcf2a
gurshivdabb <dabbgurshiv@gmail.com>
2025-11-07 09:32:33 -0600

    Add GET / route to server.js

--- CultureSwap/server.js
+++ CultureSwap/server.js
@@ -2,4 +2,8 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+app.get('/', (req, res) => {
+    res.send("Hello World from CultureSwap!")
+});
+
 app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`));
