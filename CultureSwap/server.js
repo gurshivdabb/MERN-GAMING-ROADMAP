7acff7911b0e70fb3dc3f141a3eb78023f9d4f0c
gurshivdabb <dabbgurshiv@gmail.com>
2025-11-10 23:33:19 -0600

    Initial Express server with GET and POST routes with POSTMAN

--- CultureSwap/server.js
+++ CultureSwap/server.js
@@ -2,8 +2,14 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+app.use(express.json())
+
 app.get('/', (req, res) => {
     res.send("Hello World from CultureSwap!")
 });
+
+app.post('/users', (req, res) => {
+    res.send(req.body.name + ' says hi') // CultureSwap says hi
+})
 
 app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`));
