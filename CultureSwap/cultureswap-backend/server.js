dd8335c45645cbae783cfd3ddfe8f694ad3301af
gurshivdabb <dabbgurshiv@gmail.com>
2025-11-13 10:45:24 -0600

    added mongodb and tweaked the code a bit

--- CultureSwap/cultureswap-backend/server.js
+++ CultureSwap/cultureswap-backend/server.js
@@ -1,15 +1,15 @@
-const express = require('express');
+import express from 'express';
+import connectDB from './db/connect.js';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 const app = express();
-const PORT = 3000;
+app.use(express.json());
 
-app.use(express.json())
+connectDB(); // connect to MongoDB
 
-app.get('/', (req, res) => {
-    res.send("Hello World from CultureSwap!")
-});
+app.get('/', (req, res) => res.send('Hello World!'));
 
-app.post('/users', (req, res) => {
-    res.send(req.body.name + ' says hi') // CultureSwap says hi
-})
-
-app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on PORT ${PORT}`));
